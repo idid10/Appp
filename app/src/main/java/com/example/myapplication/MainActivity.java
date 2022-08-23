bb78d4dd699fe422ddda228b06993693f84f888c
DESKTOP-LN9PSIC\82102 <ejwuud2@naver.com>
2022-08-23 15:59:49 +0900

    count

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -13,19 +13,18 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager sensorManager;
-    private Sensor stepDetectorSensor;
-    TextView tvStepDetector;
-    private int mStepDetector;
+    private Sensor stepCountSensor;
+    TextView tvStepCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tvStepDetector = (TextView)findViewById(R.id.tvStepDetector);
+        tvStepCount = (TextView)findViewById(R.id.tvStepCount);
         sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
-        stepDetectorSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
-        if(stepDetectorSensor == null) {
+        stepCountSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if(stepCountSensor == null) {
             Toast.makeText(this, "No Step Detect Sensor", Toast.LENGTH_SHORT).show();
         }
     }
@@ -33,7 +32,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     protected void onResume() {
         super.onResume();
-        sensorManager.registerListener(this, stepDetectorSensor, SensorManager.SENSOR_DELAY_UI);
+        sensorManager.registerListener(this, stepCountSensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
@@ -44,12 +43,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        if(event.sensor.getType() == Sensor.TYPE_STEP_DETECTOR) {
-            if(event.values[0] == 1.0f) {
-                mStepDetector++;
-                tvStepDetector.setText("Step Detect :" + String.valueOf(mStepDetector));
-
-            }
+        if (event.sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
+            tvStepCount.setText("Step Count : " + String.valueOf(event.values[0]));
         }
     }
 
